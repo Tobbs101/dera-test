649c69e039097822ccddb2e324f981b8a7c3a244
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-04-27 22:18:13 +0100

    Update Header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -2,39 +2,14 @@ import React from "react";
 import { Flex, Text, Box, Input } from "theme-ui";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBook } from "@fortawesome/free-solid-svg-icons";
+import { headerBar_, logo_, logoText_, logoIcon_ } from "./style.header";
 
 const Header = () => {
   return (
-    <Flex
-      sx={{
-        backgroundColor: "#fff",
-        padding: "5px 30px",
-        width: "100%",
-        border: "1px solid #ccc",
-        borderRadius: "5px",
-        boxShadow: "0 0 5px 0 #ccc",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: "5px",
-        }}
-      >
-        <Text
-          sx={{
-            fontFamily: "'Stylish', sans-serif",
-            fontSize: "28px",
-            fontWeight: "500",
-          }}
-        >
-          BOOK GALLERY
-        </Text>
-        <FontAwesomeIcon icon={faBook} className="text-secondary text-2xl" />
+    <Flex sx={headerBar_}>
+      <Box sx={logo_}>
+        <Text sx={logoText_}>BOOK GALLERY</Text>
+        <FontAwesomeIcon icon={faBook} className={logoIcon_} />
       </Box>
       <Box>
         <Input placeholder="Search..." sx={{ border: "1px solid #ccc" }} />
---
+++ src/header/style.header.js
@@ -0,0 +1,29 @@
+//HEADER CONTAINER STYLING
+export const headerBar_ = {
+  backgroundColor: "#fff",
+  padding: "5px 30px",
+  width: "100%",
+  border: "1px solid #ccc",
+  borderRadius: "5px",
+  boxShadow: "0 0 5px 0 #ccc",
+  justifyContent: "space-between",
+  alignItems: "center",
+};
+
+//LOGO STYLING
+export const logo_ = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "5px",
+};
+
+//LOGO TEXT STYLING
+export const logoText_ = {
+  fontFamily: "'Stylish', sans-serif",
+  fontSize: "28px",
+  fontWeight: "500",
+};
+
+//LOGO ICON STYLING
+export const logoIcon_ = "text-secondary text-2xl";
