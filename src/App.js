8eb44fa5e1f8da4f9dfad769f5b47f1247bd134f
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-04-27 21:01:41 +0100

    Update codebase

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ function App() {
   return (
     <div
       className="p-5"
-      style={{ backgroundColor: "#f3f5f9", minHeight: "100vh" }}
+      style={{
+        backgroundColor: "#f3f5f9",
+        minHeight: "100vh",
+      }}
     >
       <h1>Hello world</h1>
     </div>
