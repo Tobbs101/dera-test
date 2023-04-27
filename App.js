3d963f8e06ad29893b6dd84efcc3a9d145970238
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-04-27 20:52:24 +0100

    Update codebase

---
+++ App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      className="p-5"
+      style={{ backgroundColor: "#f3f5f9", minHeight: "100vh" }}
+    >
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
