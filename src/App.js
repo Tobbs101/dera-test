b37c010fa81dc8ec3a1b8952f445860341986810
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-04-27 21:36:55 +0100

    Update app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { useState } from "react";
+import { BookContext } from "./context";
 import "./App.css";
 import { Box } from "theme-ui";
 import Header from "./header/Header";
 import { Routes, Route } from "react-router-dom";
+import Books from "./books/Books";
+import BookInfo from "./bookInfo/BookInfo";
 
 function App() {
+  const [data, setData] = useState({ books: [], currentBook: {} });
   return (
-    <Box sx={{ minHeight: "100vh", backgroundColor: "paleBg" }}>
-      <Header />
-      <Routes></Routes>
-    </Box>
+    <BookContext.Provider value={{ data, setData }}>
+      <Box sx={{ minHeight: "100vh", backgroundColor: "paleBg" }}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/:id" element={<BookInfo />} />
+        </Routes>
+      </Box>
+    </BookContext.Provider>
   );
 }
 
