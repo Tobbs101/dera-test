7786b674ed71f9ccb1a10f1e78b38420e44eb9d8
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-04-27 22:12:55 +0100

    Update Header.jsx

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Flex, Text, Box } from "theme-ui";
+import { Flex, Text, Box, Input } from "theme-ui";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
@@ -11,17 +13,32 @@ const Header = () => {
         border: "1px solid #ccc",
         borderRadius: "5px",
         boxShadow: "0 0 5px 0 #ccc",
+        justifyContent: "space-between",
+        alignItems: "center",
       }}
     >
-      <Text
+      <Box
         sx={{
-          fontFamily: "'Stylish', sans-serif",
-          fontSize: "28px",
-          fontWeight: "500",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "5px",
         }}
       >
-        BOOK GALLERY
-      </Text>
+        <Text
+          sx={{
+            fontFamily: "'Stylish', sans-serif",
+            fontSize: "28px",
+            fontWeight: "500",
+          }}
+        >
+          BOOK GALLERY
+        </Text>
+        <FontAwesomeIcon icon={faBook} className="text-secondary text-2xl" />
+      </Box>
+      <Box>
+        <Input placeholder="Search..." sx={{ border: "1px solid #ccc" }} />
+      </Box>
     </Flex>
   );
 };
