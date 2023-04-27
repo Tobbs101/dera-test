8e76fd713776088cf6361ce36f570f14eb8fd63c
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-04-27 22:19:39 +0100

    Update Header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,8 +1,8 @@
+import { headerBar_, logo_, logoText_, logoIcon_ } from "./style.header";
 import React from "react";
 import { Flex, Text, Box, Input } from "theme-ui";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBook } from "@fortawesome/free-solid-svg-icons";
-import { headerBar_, logo_, logoText_, logoIcon_ } from "./style.header";
 
 const Header = () => {
   return (
