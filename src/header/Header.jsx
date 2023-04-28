f742456f3b17a69a1f4c5d972d52a87cbbf309ee
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-04-28 19:56:36 +0100

    Update Search.jsx

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,11 +1,15 @@
 import { headerBar_, logo_, logoText_, logoIcon_ } from "./style.header";
-import React from "react";
+import React, { useState } from "react";
 import { Flex, Text, Box, Select } from "theme-ui";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBook } from "@fortawesome/free-solid-svg-icons";
 import Search from "./components/Search";
 
 const Header = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  console.log("s", searchTerm);
+
   return (
     <Flex sx={headerBar_}>
       <Box sx={logo_}>
@@ -13,7 +17,7 @@ const Header = () => {
         <FontAwesomeIcon icon={faBook} className={logoIcon_} />
       </Box>
       <Flex sx={{ alignItems: "center" }}>
-        <Search />
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         <Select>
           <option></option>
         </Select>
--- src/header/components/Search.jsx
+++ src/header/components/Search.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Input } from "theme-ui";
 
-const Search = () => {
+const Search = ({ setSearchTerm }) => {
   return (
     <Input
       placeholder="Search..."
+      onChange={(e) => setSearchTerm(e.target.value)}
       sx={{
         border: "1px solid #ccc",
         outline: "none",
         height: "30px",
-        "&:focus": { border: "1px solid red" },
+        transitionDuration: ".5s",
+        "&:focus": { border: "1px solid primary" },
       }}
     />
   );
